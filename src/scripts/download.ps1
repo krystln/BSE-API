c1512730b99395a63ee84578e6d9e870e62e7bca
Akshit Goyal <91284474+Goyal-Akshit@users.noreply.github.com>
2024-01-28 01:03:22 +0530

    create /download if not available

--- src/scripts/download.ps1
+++ src/scripts/download.ps1
@@ -1,5 +1,11 @@
 $url = $args[0]
 Write-Output "Initiating download of $url"
 
+$check_path = Test-Path .\downloads
+if ($check_path -eq $false) {
+    Write-Output "Creating downloads folder"
+    New-Item -ItemType Directory -Force -Path .\downloads
+}
+
 Invoke-WebRequest $url -OutFile .\downloads\download.zip
 Write-Output "Download completed"
